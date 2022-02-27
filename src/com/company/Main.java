e4f178a44ae005d7757e6115a4628684f72199b7
Michał Tomczak <ryba971@gmail.com>
2022-02-27 10:55:57 +0100

    Zadanie 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal dog = new Animal("canis", "Szarik");
+        System.out.println("pies nazywa się " + dog.name);
+
+        System.out.println("zwierze waży " + dog.getWeight());
+        System.out.println(dog.species);
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        System.out.println("pies waży " + dog.getWeight());
+
+
+
+    }
+}
